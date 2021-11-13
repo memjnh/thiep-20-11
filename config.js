850f631494fa3d237edfb67e0c13c76965ca4c4b
memjnh <94215006+memjnh@users.noreply.github.com>
2021-11-13 14:00:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-const nameGirl = ' ';
+const nameGirl = 'Cô Tin';
 const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng Ngày Nhà Giáo Việt Nam 20-11';
+const eventName = 'Chúc Mừng 20-11';
 const titleCard = 'Tặng Cô Tin';
 const contentCard = 'Chúc cô có một ngày 20/11 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cô trong hôm nay và cả những ngày sau và thành công trên con đường giảng dạy';
 
